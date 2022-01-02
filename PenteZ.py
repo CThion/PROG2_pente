c8a5d66b6b3898780c999b5a5637f709f29589a3
ArcticWolf33600 <gwendal.prat@sfr.fr>
2022-01-02 14:42:34 +0100

    dernière séance

--- PenteZ.py
+++ PenteZ.py
@@ -12,22 +12,28 @@ class ConfigWin(Win):
   def __init__(self, dim=8, score=5, nameA='Player A', nameB='Player B'):
     """create and show the configuration window"""
     font = 'Arial 20 bold'
-    Win.__init__(self, title="ConfigWin", op=2, grow=True,bg='#98f4f3')  #config window
+    Win.__init__(self, title="ConfigWin",op=2,grow=True,bg='#98f4f3')#config window
     # --------------------------------------------------------------------------
     Label(self,text='CONFIGURATION',width=23,bg='#1b32c5',fg='#819b93',font=font)
     # --------------------------------------------------------------------------
     frame = Frame(self, fold=2, flow='ES')
     #----
-    Label(frame,text='Name of player A :',width=13,anchor='SW',grow=True,font='Arial 13 bold')
-    self.p1 = Entry(frame)
+    Label(frame,text='Name of player A :',width=13,anchor='SW',grow=True,
+          font='Arial 13 bold')
+    self.nameA = Entry(frame)
+    self.nameA.insert(0,'Player A')
     #----
-    Label(frame,text='Name of player B :',width=13,anchor='SW',grow=True,font='Arial 13 bold')
-    self.p2 = Entry(frame)
+    Label(frame,text='Name of player B :',width=13,anchor='SW',grow=True,
+          font='Arial 13 bold')
+    self.nameB = Entry(frame)
+    self.nameB.insert(0,'Player B')
     #----
-    Label(frame,text='Board Dimensions :',width=16,anchor='SW',grow=False,font='Arial 13 bold')
+    Label(frame,text='Board Dimensions :',width=16,anchor='SW',grow=False,
+          font='Arial 13 bold')
     self.dim = Scale(frame, scale=(dim, 16), flow='W', state=dim)
     #----
-    Label(frame,text='Score for Victory :',width=16,anchor='SW',grow=False,font='Arial 13 bold')
+    Label(frame,text='Score for Victory :',width=16,anchor='SW',grow=False,
+          font='Arial 13 bold')
     self.score = Scale(frame, scale=(score, 15), flow='W', state=score)
     # --------------------------------------------------------------------------
     Button(frame,text='⚙',command=self.settings,bg='#1b32c5',fg='#819b93',
@@ -36,20 +42,16 @@ class ConfigWin(Win):
            font=font)
     Button(frame,text='START',command=lambda: GameWin(self),bg='#1b32c5',
            fg='#819b93',font=font)
-    
     #---------------------------------------------------------------------------
     self.gchrono=IntVar(); self.pchrono=IntVar();self.voisinage=IntVar()
-    Button(frame, text='Show', command=lambda:print(f"""
-                                                    gchrono : {self.gchrono.get()}
-                                                    pchrono : {self.pchrono.get()}
-                                                    """))
     # --------------------------------------------------------------------------
     self.loop()
   # ----------------------------------------------------------------------------
   def settings(self):
     """callback for the "POPUP" button"""
     settingswin = Win(self, title='POPUP', flow='S', op=10,bg='#98f4f3')
-    Label(settingswin, text='SETTINGS',fg='#819b93',bg='#1b32c5', font='Arial 20 bold')
+    Label(settingswin, text='SETTINGS',fg='#819b93',bg='#1b32c5',
+          font='Arial 20 bold')
 
     frame = Frame(settingswin,flow='ES', fold=2)
     Label(frame, text='Add a global Chrono ?',font='Arial 13 bold')
@@ -58,18 +60,18 @@ class ConfigWin(Win):
     Checkbutton(frame, variable=self.pchrono)
     Label(frame, text='Add the switch rule ?',font='Arial 13 bold')
     Checkbutton(frame, variable=self.voisinage)
-    
-    #MessageDialog.askyesno('YES-NO', message='Add a global Chrono ?')
-    # self.pchrono = MessageDialog.askyesno('YES-NO', message='Add a player turn Chrono ?')    
-    # self.voisinage = MessageDialog.askyesno('YES-NO', message='Set the switch rule ?')
-    # Button(settingswin, text='Show', command=lambda:print(self.var.get()))
-    
-    
-    Button(settingswin, text='CLOSE', command = settingswin.exit, fg='#819b93',bg='#1b32c5', font='Arial 20 bold')
-
+ 
+    Button(settingswin, text='CLOSE', command = settingswin.exit,
+           fg='#819b93',bg='#1b32c5', font='Arial 20 bold')
+  # ----------------------------------------------------------------------------
   def rules(self):
     popup = Win(self, title='RULES', op=10,bg='#98f4f3')
-    Label(popup, text= "This game is called Pente \n When one of the two players reaches the chosen victory score (set before the game starts), the game is over. \n To collect points, the players can either form a 5-tokens row or column, or set 2 tokens aside an opponent's token. \n Warning ! You cannot set a token on a cell if there's already a token, or if the cell is next to a token set the turn before.")
+    Label(popup, text= "This game is called Pente \n When one of the two \
+    players reaches the chosen victory score (set before the game starts), the \
+    game is over. \n To collect points, the players can either form a 5-tokens\
+    row or column, or set 2 tokens aside an opponent's token. \n Warning ! You \
+    cannot set a token on a cell if there's already a token, or if the cell is \
+    next to a token set the turn before.")
     Button(popup,text='UNDERSTOOD',command=popup.exit,fg='#819b93',bg='#1b32c5',
            font='Arial 20 bold')
     popup.wait()
@@ -89,8 +91,8 @@ class GameWin(Win):
     # --------------------------------------------------------------------------
     self.dim = config.dim.state
     self.game = Game(self.dim)  # create kernel class and store it as attribute
-    self.NameA = config.p1.state  #'NAME A'
-    self.NameB = config.p2.state  #'NAME B'
+    self.NameA = config.nameA.state  #'NAME A'
+    self.NameB = config.nameB.state  #'NAME B'
     self.score = config.score.state
     # --------------------------------------------------------------------------
     config.exit()
@@ -190,7 +192,8 @@ class GameWin(Win):
         Label(frame, text = f"{self.chrono1['text']} second(s) remaining")
         Label(frame, text = f"{self.chrono2['text']} second(s) remaining")
       # --//--
-      Button(popup,text='NEW GAME',command=self.new_game,bg='#1b32c5', fg='#819b93',
+      Button(popup,text='NEW GAME',command=self.new_game,bg='#1b32c5',
+             fg='#819b93',
              font='Arial 20 bold')
       popup.wait()
   # ----------------------------------------------------------------------------
@@ -202,23 +205,20 @@ class GameWin(Win):
   def tick(self):
     """Manage chrono"""
     self.victory()  #stop the chrono if there's a winner
-
+    #----Player chrono only
     if self.pchrono==True :
       if self.chrono1['text']==0 :
         self.game.over=True
         self.winner = self.NameB
-        self.victory()
         return
       if self.chrono2['text']==0:
         self.game.over=True
         self.winner=self.NameA
-        self.victory()
         return
-
+    #----Global chrono only
     if self.gchrono == True and self.pchrono == False:
       self.globalchrono['text']+=1
       self.after(1000,self.tick)
-
     elif self.gchrono == False and self.pchrono == True:
       if self.tour.state == 0:
         self.chrono1['text'] = self.chrono1['text'] -1
@@ -228,7 +228,7 @@ class GameWin(Win):
         self.chrono2['text'] = self.chrono2['text'] -1 
         if self.chrono2['text'] == 20: self.chrono2['fg'] = 'red'
         self.after(1000, self.tick)
-
+    #----Both chrono
     elif self.gchrono==True and self.pchrono==True:
       self.globalchrono['text']+=1
       if self.tour.state == 0:
@@ -251,12 +251,10 @@ class Game(object):
     self.Changes = []  #list of points modified by current move
     self.score = [0, 0]  #storage of players score
     self.playerID = 2  #last player who played, 2 by default for the first move
-
   # ----------------------------------------------------------------------------
   def __call__(self, row, col, state=None):
     """get or set state for provided grid cell. Control cell validity (in grid)"""
     if not 0 <= row < self.dim or not 0 <= col < self.dim: #out of grid
-        #print('out of grid')
         return 
     if state == None: 
         return self.MState[row][col]  #return cell state
@@ -264,7 +262,6 @@ class Game(object):
       assert isinstance(state, int), "state must be an integer"
       self.MState[row][col] = state % 4
       self.Changes.append((row, col))
-
   # ----------------------------------------------------------------------------
   def switch(self, row, col, valid=True):
     """switch valid/invalid state for neighborhood of provided grid cell"""
@@ -282,28 +279,22 @@ class Game(object):
   # ----------------------------------------------------------------------------
   def align(self, row, col, playerID):
     """check if provided move creates align config and return score update"""
-    print('----ALIGN----')
-    neighborhood = [[0, 1], [1, 0], [-1, 1], [1, 1]] #-> v <- ^
-                    #(0, -1), (-1, 1), (-1, 0), (-1, -1)]
+    neighborhood = [[0, 1], [1, 0], [-1, 1], [1, 1]]     
     for x in neighborhood:
         line=[self(row+(-5+k)*x[0], col+(-5+k)*x[1]) for k in range(1,10)]
         linestr=''.join([str(l) for l in line])
-        print('linestr : ', linestr)
         if 5*str(playerID) in linestr: 
-            print("nice")
             self.score[playerID - 1] += 5 #update score
-  
   # ----------------------------------------------------------------------------
   def capture(self, row, col, playerID):
     """check if provided move creates capture config and return score update"""
     adversaryID = 2-(1+playerID)%2
-    neighborhood = [(0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1)]
+    neighborhood=[(0, 1),(-1, 1),(-1, 0),(-1, -1),(0, -1),(1, -1),(1, 0),(1, 1)]
     for x in neighborhood:
       line=[self(row+k*x[0], col+k*x[1]) for k in range(4)]
       if line == [playerID, adversaryID, adversaryID, playerID]:
           self.score[playerID - 1] += 1 #update score
-          for k in range(1,3): self(row+k*x[0], col+k*x[1], 0) #delete advers captured token
-          
+          for k in range(1,3):self(row+k*x[0],col+k*x[1],0)#delete advers captured token
 # ==============================================================================
 if __name__ == "__main__":
   ConfigWin()
