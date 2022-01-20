5c6b6aff6d63459f0af4947697f0335b06dfb20b
Clément Thion <clement.thion@orange.fr>
2022-01-20 13:56:40 +0100

    Update test_code_Game.py

--- test_code_Game.py
+++ test_code_Game.py
@@ -8,15 +8,20 @@ Created on Fri Jan  7 16:25:16 2022
 from PenteZ import Game
 from ezCLI import grid, testcode 
 
-#==============TEST CODE FOR kernel class Game ==============
+#==============TEST CODE FOR kernel class Game ================================
 class GameDemo(Game):
-    """demonstration class for Game class. Add methode for test of Game code"""
+    """demonstration class for Game class. Added few methodes for test of Game code"""
     def __repr__(self):
+        """give main information about current game"""
         repre = f"joueur 1 : {self.score[0]} | joueur 2 : {self.score[1]} | joueur courrant : {self.playerID}"
         return repre
+    #--------------------------------------------------------------------------
     def __str__(self):
+        """representation of the grid with ezCLI.grid"""
         return grid(self.MState, label=True, size=3)
+    #--------------------------------------------------------------------------
     def on_clic(self, row, col):
+        """simulation of player click"""
         playerID = self.playerID 
         if self(row, col)!=0 : return
         self(row, col, playerID)
@@ -35,7 +40,7 @@ class GameDemo(Game):
      
         
 #==============================================================================
-
+#matrice "jouets" préremplies pour les tests
 MtestAlign=[
       [1,0,0,0,0,0,0],
       [2,1,1,1,0,1,0],
@@ -47,62 +52,63 @@ MtestAlign=[
     ]
 B=A=0
 MtestCapture=[
-    [2,0,0,1,2,B,1],#0,5
+    [2,0,0,1,2,B,1],
     [0,1,0,0,0,0,0],
     [0,0,1,0,1,0,0],
-    [0,0,0,B,0,0,1],#3,3
+    [0,0,0,B,0,0,1],
     [0,0,2,0,0,0,2],
-    [2,A,1,B,0,0,2],#5,1 ; 5,3
-    [0,0,0,1,2,2,A],#6,6
+    [2,A,1,B,0,0,2],
+    [0,0,0,1,2,2,A],
     ]
 
 code=f"""
 #--------__init__--------
-p = GameDemo(7)
-print(p)
-p
+p = GameDemo(7) #instance de GameDemo
+print(p) #test de __str__
+p #test de __call__
 
 #---------__call__--------
 print(p(6,0))
-p(6,0,2)
+p(6,0,2) #modification de la valeur en 6,0
 print(p, p(6,0))
 
 #--------rules--------
-p.on_clic(1,1); print(p)
-
-p.on_clic(3,3); print(p)
-
 #----switch----
-p.on_clic(3,4); p.on_clic(3,3)
+p.on_clic(1,1); print(p) #mise en évidence de l'application de switch
+
+p.on_clic(3,3); print(p) #l'ancien voisinage est bien effacé
+
+p.on_clic(3,4); p.on_clic(3,3) #on ne peut pas modifier un state = 3
 print(p)
-print(p.history)
+
+print(p.history) # l'hystorique des points est bien mis à jour
 
 #----align----
-p = GameDemo(7)
-p.MState=MtestAlign
+p = GameDemo(7) #nouvel objet pour la suite des tests
+p.MState=MtestAlign #utilisation de la matrice pré-remplie
 print(p)
 
-p.on_clic(5,0) #vertical
-print(p)
-p
-
-p.on_clic(1,4) #horizontal
+p.on_clic(5,0) #alignement vertical
 print(p)
 p
 
-p.on_clic(6,5) #double
+p.on_clic(1,4) #alignement horizontal
 print(p)
 p
 
-p.on_clic(0,6) #diagonal up
+p.on_clic(6,5) #double, on peut bien réaliser deux alignements en même temps
 print(p)
 p
 
-p.on_clic(6,0) #vertical again
+p.on_clic(0,6) #diagonal montante vers la droite
 print(p)
 p
 
-p.on_clic(2,2) #diagonal down
+p.on_clic(6,0) #réutilisation d'un alignement déjà utilisé
+print(p)
+p
+
+p.on_clic(2,2) #diagonal descendante vers la droite
 print(p)
 p
 
@@ -111,11 +117,11 @@ p = GameDemo(7)
 p.MState=MtestCapture
 print(p)
 
-p.on_clic(3,3) #diagonal down
+p.on_clic(3,3) #diagonal descendante
 print(p)
 p
 
-p.on_clic(5,1) #diagonal up
+p.on_clic(5,1) #diagonal monte
 print(p)
 p
 
@@ -127,7 +133,7 @@ p.on_clic(6,6) #vertical (double)
 print(p)
 p
 
-p.on_clic(0,5) #not working
+p.on_clic(0,5) #un joueur de peut pas s'auto-capturer entre deux pions de l'adversaire
 print(p)
 p
 """
